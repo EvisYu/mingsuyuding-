1b76248045c97b2615e656939e5f3ac2318995fd
EvisYu <1527852901@qq.com>
2024-05-21 22:36:31 +0800

    11

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,18 +1,19 @@
-// 'use client'
+'use client'
 
 // import Image from "next/image"
-// import { useRouter } from "next/navigation"
-// export default function Logo() {
-//     const router = useRouter()
+import { useRouter } from "next/navigation"
+export default function Logo() {
+    const router = useRouter()
 
-//   return (
-//     <Image
-//       onClick={()=> router.push('/')}
-//       alt="logo"
-//       className="hidden md:block cursor-pointer"
-//       height='100'
-//       width='100'
-//       src='/images/logo.png'
-//     />
-//   )
-// }
+  return (
+    // <Image
+    //   onClick={()=> router.push('/')}
+    //   alt="logo"
+    //   className="hidden md:block cursor-pointer"
+    //   height='100'
+    //   width='100'
+    //   src='/images/logo.png'
+    // />
+    <div></div>
+  )
+}
